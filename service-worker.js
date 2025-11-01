feec194f2201ba4ea73ef22ee318fc93e94303d8
Dalavi Sudarshan Tushar <105687399+sudarshan0777@users.noreply.github.com>
2025-11-01 19:10:31 +0530

    Update service worker to use new cache strategy

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,22 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'pupilify-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('pupilify-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
